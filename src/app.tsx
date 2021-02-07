ff0e3eb1555a76a5b6a8b1713a93deb75a7c1c4b
Felipe <el_biker_felipebm@hotmail.com>
2021-02-07 12:34:53 +0100

    creando componente grid
    creando componente grid

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import Grid from './components/grid';
 
 function render() {
-  ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  //ReactDOM.render(<h2>Hello from React!</h2>, document.body);
+  ReactDOM.render(<Grid />, document.querySelector('#grid'));
 }
 
 render();
