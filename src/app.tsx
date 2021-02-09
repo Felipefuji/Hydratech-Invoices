691e1dcf432763cf25099f3de8071ba8743e3414
claudia <clabermar@outlook.es>
2021-02-09 20:25:33 +0100

    Create-menu
    He creado el menu desplegable

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Grid from './components/grid';
+import Menu from './components/menu';
 
 function render() {
-  //ReactDOM.render(<h2>Hello from React!</h2>, document.body);
-  ReactDOM.render(<Grid />, document.querySelector('#grid'));
+  //ReactDOM.render(<Grid />, document.querySelector('#grid'));
+  ReactDOM.render(<Menu />, document.getElementById('menu'));
 }
 
 render();
